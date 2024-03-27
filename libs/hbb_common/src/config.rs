5f3e156ea6ef04b2142429edea4c77bc076b7438
zhuzhu88920 <101244753+zhuzhu88920@users.noreply.github.com>
2024-03-28 01:56:23 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,8 +101,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.zdf8.eu.org"];
-pub const PUBLIC_RS_PUB_KEY: &str = "I1h3pA2yB3as08AKjkbsjfBh+2+p0lPVMNnqr5iszyE=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
